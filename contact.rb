8f80a4480a6c6b653e76df2169b07202b49e71f8
s770am <s770am@gmail.com>
2021-07-07 08:40:57 -0400

    add database requirements

--- contact.rb
+++ contact.rb
@@ -1,3 +1,11 @@
+gem 'activerecord' , '=4.2.10'
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: "crm.sqlite3") 
+
+
+
 class Contact
 
   # This method should initialize the contact's attributes
